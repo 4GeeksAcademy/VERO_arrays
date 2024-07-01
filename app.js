0c5b2babb4a7217b9980626373dfc29392a278ef
Vero519 <voviedo6@gmail.com>
2024-07-01 14:55:44 +0000

    primer_array

---
+++ app.js
@@ -0,0 +1,22 @@
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+
+
+
+for(let i=0;i<pronoun.length;i++){
+    //  console.log(pronoun[i])
+    for (let j = 0; j < adj.length; j++){
+      // console.log(adj[j]) 
+        for(let h=0;h<noun.length;h++){
+        //    console.log(noun[k]) 
+           console.log (pronoun[i]+adj[j]+ noun[h]+".com");
+            }
+
+    }     
+
+    }
+
+
+
+
